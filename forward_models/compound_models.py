7a8fd987755a184ae295cb3952cf5fc1fd3b2719
Konstantin Kharitonov <kharitok@max-wng024.desy.de>
2023-01-24 17:16:36 +0100

    Variable noise added

--- forward_models/compound_models.py
+++ forward_models/compound_models.py
@@ -4,7 +4,7 @@ from typing import Tuple
 import torch as th
 
 from .ptychography_models import PtychographyModelTransmission
-from .noise_models import AdditiveGaussianNoise
+from .noise_models import AdditiveGaussianNoise,AdditiveGaussianNoiseVariable
 
 
 class TransmissionPtychographyWithGaussianNoise(th.nn.Module):
@@ -17,6 +17,41 @@ class TransmissionPtychographyWithGaussianNoise(th.nn.Module):
             **ptychography_parameters
         )
         self.noise_model = AdditiveGaussianNoise(**noise_parameters)
+
+    def forward(self, scan_numbers) -> Tuple:
+        """Estimate the measured diffraction patterns for corresponding scan numbers,
+        considering noise, detector, etc.
+
+        """
+        #         print("Sample", self.Sample().shape)
+        #         print("Probe", self.Probe(scan_numbers).shape)
+        #         print("scan_numbers", scan_numbers.shape)
+        return (
+            th.sqrt(th.sum(th.abs(self.ptychography_model(scan_numbers)) ** 2, axis=1)),
+            self.noise_model.get_gaussian(),
+        )
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            try:
+                return getattr(self.ptychography_model, name)
+            except AttributeError:
+                return getattr(self.noise_model, name)
+
+
+
+class TransmissionPtychographyWithVariableGaussianNoise(th.nn.Module):
+    """Integrates noise and experiment model for transmissive prychoraphy with gaussian noise
+    """
+
+    def __init__(self, ptychography_parameters: dict, noise_parameters: dict) -> None:
+        super().__init__()
+        self.ptychography_model = PtychographyModelTransmission(
+            **ptychography_parameters
+        )
+        self.noise_model = AdditiveGaussianNoiseVariable(**noise_parameters)
 
     def forward(self, scan_numbers) -> Tuple:
         """Estimate the measured diffraction patterns for corresponding scan numbers,
